e24e563f9d1966597bb3cd711bf9e2c933eb3179
zhenhongswell <zhenhongswell@gmail.com>
2023-11-20 14:21:21 +0800

    add operator function calls add,subtract,multply,divide function and return result

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 
-let firstNumber = 0;
-let SecondNumber = 0;
-let operator = "";
+let firstNumber_ = 0;
+let SecondNumber_ = 0;
+let operator_ = "";
 
 //number operator number
 // 3 + 2
+
+function operator(firstNumber,operator,secondNumber){
+    let result = 0;
+    switch(operator){
+        case '+':
+            result = add(firstNumber,secondNumber);
+            break;
+        case '-':
+            result = subtract(firstNumber,secondNumber);
+            break;
+        case 'x':
+            result = multiply(firstNumber,secondNumber);
+            break;
+        case '/':
+            result = divide(firstNumber,secondNumber);
+            break;
+    }
+    return result;
+}
+
 function add(a,b){
     return a+b;
 }
