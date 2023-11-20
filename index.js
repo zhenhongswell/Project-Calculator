495c6f22aafc30c681645fbc472d58da5e34baa8
zhenhongswell <zhenhongswell@gmail.com>
2023-11-20 14:07:30 +0800

    basic set up.

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//number operator number
+// 3 + 2
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
